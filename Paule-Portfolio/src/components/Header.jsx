60841be8573d609d94f666b36c3ef2005e13f6d7
Moises <macmoises12@gmail.com>
2025-05-19 22:59:34 +0800

    added mypicture adjusted text effects.

--- Paule-Portfolio/src/components/Header.jsx
+++ Paule-Portfolio/src/components/Header.jsx
@@ -1,9 +1,19 @@
+import React, { useState, useEffect } from 'react';
 import '../css/Header.css'
 import linkedin from '../assets/Header/linkedin.png'
 import github from '../assets/Header/github.png'
 import contact from '../assets/Header/contact.png'
 
 function Header() {
+    const [showFirst, setShowFirst] = useState(true);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setShowFirst(prev => !prev);
+        }, 2000); // Change image every 2 seconds
+        return () => clearInterval(interval);
+    }, []);
+
     return(
         <div className='header'>
             
@@ -32,9 +42,16 @@ function Header() {
             </div>
 
             <div id="light">
-            <div id="lineh1"></div>
-            <div id="lineh2"></div>
-         
+                <div id="lineh1"></div>
+                <div id="lineh2"></div>
+            </div>
+
+            <div className='picture-container'>
+                <img
+                    src={showFirst ? "moises.png" : "alden.png"}
+                    alt='profile'
+                    className={`fade-image ${showFirst ? 'show' : 'hide'}`}
+                />
             </div>
 
         </div>
