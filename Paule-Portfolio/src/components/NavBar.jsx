a9885975289ae8f2ba606d60fa73446eeb7cb0f1
Moises <macmoises12@gmail.com>
2025-05-22 18:00:41 +0800

    Hover animations added.

--- Paule-Portfolio/src/components/NavBar.jsx
+++ Paule-Portfolio/src/components/NavBar.jsx
@@ -25,7 +25,7 @@ function NavBar() {
             <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
                 <li><a href="#work">Work</a></li>
                 <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+          
       
             </ul>
          </nav>
