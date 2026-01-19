d12da53d03858cebf93cf4b2adf43ac78d9edcb3
Moises <macmoises12@gmail.com>
2026-01-19 17:02:00 +0800

    added import speed on main

--- Paule-Portfolio/src/main.jsx
+++ Paule-Portfolio/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
-    <App />
+    <>
+      <App />
+      {import.meta.env.MODE === 'development' && <SpeedInsights />}
+    </>
   </StrictMode>,
 )
