44ce958d740b9ab61ba740b01148b857eb750b11
MacMoises <155962034+Macc-Mp@users.noreply.github.com>
2026-07-06 00:54:05 +0800

    Update URL in test to use 127.0.0.1

--- Paule-Portfolio/tests/example.spec.js
+++ Paule-Portfolio/tests/example.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('My Portfolio Loads Successfully', async ({ page }) => {
   // 1. Go to your local Vite server (Change the port number if yours is different!)
-  await page.goto('http://localhost:5173');
+  await page.goto('http://127.0.0.1:5173');
 
   // 2. Assert that your main heading or portfolio title is visible
   // Change "MYPORTFOLIO" to an actual text snippet visible on your homepage
